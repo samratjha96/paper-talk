ce069b8e0b648c7a721fd2e9f87ff56f75eb5dbc
Samrat Jha <samratj@amazon.com>
2023-10-08 18:04:29 -0400

    bug: fix the URL for TRPC

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -3,15 +3,16 @@
 import { PropsWithChildren, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { trpc } from "@/app/_trpc/client";
-import { httpBatchLink } from "@trpc/client";
+import { httpBatchLink, loggerLink } from "@trpc/client";
 import { absoluteUrl } from "@/lib/utils";
 
 const Providers = ({children}: PropsWithChildren) => {
   const [queryClient] = useState(() => new QueryClient())
   const [trpcClient] = useState(() => trpc.createClient({
     links: [
+      loggerLink(),
       httpBatchLink({
-        url: absoluteUrl("api/trpc")
+        url: absoluteUrl("/api/trpc")
       })
     ],
     })
