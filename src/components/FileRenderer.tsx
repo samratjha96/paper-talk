e7af750d35a539ecee78d973a0cb89be8a0637b4
Samrat Jha <samratj@amazon.com>
2023-10-08 14:44:49 -0400

    chore: Fix rendering for resizing screens

--- src/components/FileRenderer.tsx
+++ src/components/FileRenderer.tsx
@@ -1,35 +1,55 @@
-"use client"
+"use client";
 
-import React from "react"
-import { Document, Page, pdfjs } from "react-pdf"
-import 'react-pdf/dist/Page/AnnotationLayer.css'
-import 'react-pdf/dist/Page/TextLayer.css'
+import { Loader2 } from "lucide-react";
+import React from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+import { useToast } from "./ui/use-toast";
+import { useResizeDetector } from "react-resize-detector"
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 interface FileRendererProps {
-  url: string
+  url: string;
 }
 
 const FileRenderer = ({ url }: FileRendererProps) => {
+  const { toast } = useToast();
+
+  const { width, ref } = useResizeDetector()
+
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
-        <div className="flex items-center gap-1.5">
-          Top bar
-        </div>
+        <div className="flex items-center gap-1.5">Top bar</div>
       </div>
 
       {/* Main render */}
       <div className="flex-1 w-full max-h-screen">
-        <div>
-          <Document file={url} className="max-h-full">
-            <Page pageNumber={1} />
+        <div ref={ref}>
+          <Document
+            loading={
+              <div className="flex justify-center">
+                <Loader2 className="my-24 h-6 w-6 animate-spin" />
+              </div>
+            }
+            onLoadError={() => {
+              toast({
+                title: "Error loading PDF",
+                description: "Please try against later",
+                variant: "destructive",
+              });
+            }}
+            file={url}
+            className="max-h-full"
+          >
+            <Page width={width ? width: 1} pageNumber={1} />
           </Document>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FileRenderer
+export default FileRenderer;
