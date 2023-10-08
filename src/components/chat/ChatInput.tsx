ed9c7b115afa5d4421b8ec7bac2c176a4ee34236
Samrat Jha <samratj@amazon.com>
2023-10-08 19:58:23 -0400

    feat: Add custom scrollbar in text area

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Textarea } from "../ui/textarea"
 
 interface ChatInputProps {
   isDisabled?: boolean
@@ -6,8 +7,23 @@ interface ChatInputProps {
 
 const ChatInput = ( { isDisabled }: ChatInputProps) => {
   return (
-    <div>
-      
+    <div className="absolute bottom-0 lef-0 w-full">
+      <form action="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+        <div className="relative flex h-full flex-1 items-stretch md:flex-col">
+          <div className="relative flex flex-col w-full flex-grow p-4">
+            <div className="relative">
+              <Textarea 
+              rows={1} 
+              maxRows={4} 
+              autoFocus
+              placeholder="Enter your question..."
+              className="resize-none pr-12 text-base py-3 scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
+              >
+              </Textarea>
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   )
 }
