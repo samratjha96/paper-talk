28a81b2cd6a186ae94480a535c0b991850e0f730
Samrat Jha <samratj@amazon.com>
2023-10-08 14:08:37 -0400

    feat: Render PDF using react-pdf

--- src/components/FileRenderer.tsx
+++ src/components/FileRenderer.tsx
@@ -1,11 +1,31 @@
-import React from "react"
+"use client"
 
-const FileRenderer = () => {
+import React from "react"
+import { Document, Page, pdfjs } from "react-pdf"
+import 'react-pdf/dist/Page/AnnotationLayer.css'
+import 'react-pdf/dist/Page/TextLayer.css'
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+
+interface FileRendererProps {
+  url: string
+}
+
+const FileRenderer = ({ url }: FileRendererProps) => {
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
         <div className="flex items-center gap-1.5">
           Top bar
+        </div>
+      </div>
+
+      {/* Main render */}
+      <div className="flex-1 w-full max-h-screen">
+        <div>
+          <Document file={url} className="max-h-full">
+            <Page pageNumber={1} />
+          </Document>
         </div>
       </div>
     </div>
