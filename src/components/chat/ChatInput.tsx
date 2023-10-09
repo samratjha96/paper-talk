40f16b04959724ce0da55d4e6990ed970a065615
Samrat Jha <samratj@amazon.com>
2023-10-08 20:20:22 -0400

    feat: Add handler for sending message and clean up ChatInput

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 import { Textarea } from "../ui/textarea"
+import { Button } from "../ui/button"
+import { Send } from "lucide-react"
 
 interface ChatInputProps {
   isDisabled?: boolean
@@ -7,7 +9,7 @@ interface ChatInputProps {
 
 const ChatInput = ( { isDisabled }: ChatInputProps) => {
   return (
-    <div className="absolute bottom-0 lef-0 w-full">
+    <div className="absolute bottom-0 left-0 w-full">
       <form action="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
         <div className="relative flex h-full flex-1 items-stretch md:flex-col">
           <div className="relative flex flex-col w-full flex-grow p-4">
@@ -18,8 +20,11 @@ const ChatInput = ( { isDisabled }: ChatInputProps) => {
               autoFocus
               placeholder="Enter your question..."
               className="resize-none pr-12 text-base py-3 scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
-              >
-              </Textarea>
+              />
+
+              <Button aria-label="send message" className="absolute bottom-1.5 right-[8px]">
+                <Send className="h-4 w-4" />
+              </Button>
             </div>
           </div>
         </div>
